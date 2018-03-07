37cd8dba2855fdc2a016aec5e09b35bd94870b61
Indrek Paas <indrekpaas@users.noreply.github.com>
2018-03-07 15:09:08 +1100

    Add culture template

---
+++ README.md
@@ -0,0 +1,28 @@
+# Qantas – Osaka (Interactive)
+
+## Development
+
+```
+npm install
+npm start
+```
+
+## Staging
+
+```
+npm run stage
+```
+
+## Production
+
+### Build locally
+
+```
+npm run build
+```
+
+### Deploy to CDN
+
+```
+npm run deploy
+```
--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,67 @@
+const config = require('./config.json')
+
+const template = 'culture'
+// const template = 'explore'
+// const template = 'food'
+
 const autoprefixer = require('gulp-autoprefixer')
 const browserSync = require('browser-sync')
 const concat = require('gulp-concat')
 const del = require('del')
+const file = require('gulp-file')
 const gulp = require('gulp')
+const gulpIf = require('gulp-if')
 const htmlmin = require('gulp-htmlmin')
-const imagemin = require('gulp-imagemin')
 const nunjucks = require('gulp-nunjucks')
 const plumber = require('gulp-plumber')
 const rename = require('gulp-rename')
 const runSequence = require('run-sequence')
+const s3Upload = require('gulp-s3-upload')
 const sass = require('gulp-sass')
 const sassVars = require('gulp-sass-vars')
 const sourcemaps = require('gulp-sourcemaps')
 const surge = require('gulp-surge')
-const svgo = require('gulp-svgo')
-const svgstore = require('gulp-svgstore')
+const tinypng = require('gulp-tinypng')
 const uglify = require('gulp-uglify')
 
-const cdn = null
+let domain = path = project = version = cdn = null
+
+if (process.env.NODE_ENV === 'production') {
+  domain = 'https://interactive.guim.co.uk'
+  path = 'atoms'
+  project = `${config.path}-${template}`
+  version = `v/${Date.now()}`
+  cdn = `${domain}/${path}/${project}/${version}/`
+}
+
+function s3(cacheControl, keyPrefix) {
+  return s3Upload()({
+    'Bucket': 'gdn-cdn',
+    'ACL': 'public-read',
+    'CacheControl': cacheControl,
+    'keyTransform': fn => `${keyPrefix}/${fn}`
+  })
+}
+
+gulp.task('aws', () =>
+  gulp.src([
+    'dest/**/*',
+    '!dest/**/*.map'
+  ])
+    .pipe(s3('max-age=31536000', `${path}/${project}/${version}`))
+    .on('end', () =>
+      gulp.src('config.json')
+        .pipe(file('preview', version))
+        .pipe(file('live', version))
+        .pipe(s3('max-age=30', `${path}/${project}`))
+    )
+)
 
 gulp.task('browser-sync', () =>
   browserSync.init({
     server: {
       baseDir: 'dest',
-      directory: true
+      index: 'main.html'
     }
   })
 )
@@ -36,26 +74,17 @@ gulp.task('clean', () =>
   del('dest')
 )
 
-gulp.task('build:files', ['icons', 'images', 'scripts', 'stylesheets', 'templates'])
+gulp.task('build:files', ['images', 'scripts', 'stylesheets', 'templates'])
 
 gulp.task('default', ['build', 'watch'])
 
-gulp.task('icons', () =>
-  gulp.src('src/icons/*.svg')
-    .pipe(rename({
-      prefix: 'icon-'
-    }))
-    .pipe(svgo())
-    .pipe(svgstore({
-      inlineSvg: true
-    }))
-    .pipe(gulp.dest('dest/images'))
-    .on('end', browserSync.reload)
+gulp.task('deploy', callback =>
+  runSequence('build', 'aws', callback)
 )
 
 gulp.task('images', () =>
   gulp.src('src/images/*')
-    .pipe(imagemin())
+    .pipe(gulpIf(process.env.NODE_ENV === 'production', tinypng('hVF5cDJ6l3HWdKS6-Unkd5mb0RUuJcVg')))
     .pipe(gulp.dest('dest/images'))
     .on('end', browserSync.reload)
 )
@@ -68,17 +97,16 @@ gulp.task('public', () =>
 gulp.task('scripts', () =>
   gulp.src([
     'node_modules/flickity/dist/flickity.pkgd.js',
-    'node_modules/svg4everybody/dist/svg4everybody.js',
     'node_modules/what-input/dist/what-input.js',
     'node_modules/when-in-viewport/dist/whenInViewport.js',
     'src/scripts/*.js'
   ])
     .pipe(plumber())
     .pipe(sourcemaps.init())
-      .pipe(concat('index.js'))
+      .pipe(concat('main.js'))
       .pipe(uglify())
     .pipe(sourcemaps.write(''))
-    .pipe(gulp.dest('dest/scripts'))
+    .pipe(gulp.dest('dest'))
     .on('end', browserSync.reload)
 )
 
@@ -99,7 +127,7 @@ gulp.task('stylesheets', () =>
       }))
       .pipe(autoprefixer())
     .pipe(sourcemaps.write(''))
-    .pipe(gulp.dest('dest/stylesheets'))
+    .pipe(gulp.dest('dest'))
     .pipe(browserSync.stream({
       match: '**/*.css'
     }))
@@ -108,12 +136,12 @@ gulp.task('stylesheets', () =>
 gulp.task('surge', () =>
   surge({
     project: 'dest',
-    domain: 'qantas-osaka-interactive.surge.sh'
+    domain: `qantas-osaka-interactive-${template}.surge.sh`
   })
 )
 
 gulp.task('templates', () =>
-  gulp.src('src/templates/*.njk')
+  gulp.src(`src/templates/${template}.njk`)
     .pipe(plumber())
     .pipe(nunjucks.compile({
       cdn: cdn
@@ -123,6 +151,7 @@ gulp.task('templates', () =>
       removeComments: true
     }))
     .pipe(rename({
+      basename: 'main',
       extname: '.html'
     }))
     .pipe(gulp.dest('dest'))
@@ -130,7 +159,6 @@ gulp.task('templates', () =>
 )
 
 gulp.task('watch', ['browser-sync'], () => {
-  gulp.watch('src/icons/*.svg', ['icons'])
   gulp.watch('src/images/*', ['images'])
   gulp.watch('src/scripts/*.js', ['scripts'])
   gulp.watch('src/stylesheets/**/*.scss', ['stylesheets'])
